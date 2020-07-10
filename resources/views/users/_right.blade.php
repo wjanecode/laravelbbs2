2ff5e0681737c5cc649ff984e339e836b5a2dada
woojuan <woojuan163@163.com>
2020-07-10 15:26:54 +0800

    '准备生成reply'

--- resources/views/users/_right.blade.php
+++ resources/views/users/_right.blade.php
@@ -2,7 +2,12 @@
 
 <div class="card">
   <div class="card-body">
-   @include('posts._post_list',['posts'=>$user->posts()->with('category','user')->paginate(10)])
+    <ul class="nav nav-tabs">
+      <li class="nav item"><a href="" class="nav-link active">@if(Auth::check()) 我 @else Ta @endif的回复</a></li>
+      <li class="nav item"></li>
+    </ul>
+    @include('posts._post_list',['posts'=>$user->posts()->with('category','user')->paginate(10)])
+
 
   </div>
 </div>
